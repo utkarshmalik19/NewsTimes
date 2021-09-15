9a1decb007b0c4765d51cc3df2a58ab1995b0447
Utkarsh <utkarshmalik19@gmail.com>
2021-09-15 22:18:42 +0530

    NewsTimes App

---
+++ app/src/main/java/com/example/newstimes/OnFetchDataListener.java
@@ -0,0 +1,10 @@
+package com.example.newstimes;
+
+import com.example.newstimes.Models.NewsHeadlines;
+
+import java.util.List;
+
+public interface OnFetchDataListener<NewsApiResponse> {
+    void onFetchData(List<NewsHeadlines> list, String message);
+    void onError(String message);
+}
---
+++ app/src/main/java/com/example/newstimes/SelectListener.java
@@ -0,0 +1,8 @@
+package com.example.newstimes;
+
+import com.example.newstimes.Models.NewsHeadlines;
+
+public interface SelectListener {
+    void OnNewsClicked(NewsHeadlines headlines);
+}
+
